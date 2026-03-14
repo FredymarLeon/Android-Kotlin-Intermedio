42616dbff6f4bdb6c7085226ee6d2d0f2e12eee9
FredymarLeon <fredymar@gmail.com>
2026-03-14 09:12:32 -0300

    buildTypes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,20 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            resValue("string", "fredymarname", "HoroscApp")
+            buildConfigField("String", "BASE_URL", "\" https ://newastro.vercel.app/\"")
+        }
+        getByName("debug") {
+            isDebuggable = true
+            resValue("string", "fredymarname", "[DEBUG] HoroscApp")
+            buildConfigField("String", "BASE_URL", "\" https ://newastro-debug.vercel.app/\"")
         }
     }
     compileOptions {
@@ -38,6 +46,7 @@ android {
     }
     buildFeatures{
         viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -64,6 +73,7 @@ dependencies {
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.3.1")
 
 
 }
